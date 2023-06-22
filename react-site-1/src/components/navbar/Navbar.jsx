e729f3f78f9e236ddc5e0378bc9f29907dd9c728
Akshat-shah05 <akshatshah1605@gmail.com>
2023-06-21 23:14:50 -0400

    Downloaded all the assets needed (images/logos)

--- react-site-1/src/components/navbar/Navbar.jsx
+++ react-site-1/src/components/navbar/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 import './navbar.css';
+
+// BEM --> block element modifier for css naming
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo"></img>
+        </div>
+      </div>
     </div>
   )
 }
